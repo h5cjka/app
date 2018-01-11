c56a1340d9f679aec03d73f0af06d88b73c36467
胡杰豪 <hujiehao@hongrongtech.com>
2018-01-11 17:50:54 +0800

    update

--- static/sw/firebase-messaging.js
+++ static/sw/firebase-messaging.js
@@ -1,27 +1,4 @@
 module.exports = `
   <script src=https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js></script>
   <script src=https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js></script>
-  <script>
-    const options = {
-      messagingSenderId: '48744293829'
-    }
-
-    firebase.initializeApp(options)
-
-    const messaging = firebase.messaging()
-    messaging.requestPermission()
-    .then(() => {
-      console.log('已开启通知')
-      return messaging.getToken()
-    })
-    .then(token => {
-      console.log(token)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-    messaging.onMessage(payload => {
-      console.log('Message received', payload)
-    })
-  </script>
 `
--- static/sw/main.js
+++ static/sw/main.js
@@ -1,4 +1,5 @@
 /* eslint-env worker, serviceworker */
+/* eslint-disable no-undef */
 'use strict'
 
 const isLocalhost = Boolean(
@@ -9,10 +10,33 @@ const isLocalhost = Boolean(
   self.location.hostname === '[::1]'
 )
 
+firebase.initializeApp({messagingSenderId: '48744293829'})
+
+let messaging = firebase.messaging()
+messaging.requestPermission()
+  .then(() => {
+    console.log('已开启通知')
+    return messaging.getToken()
+  })
+  .then(token => {
+    console.log(token)
+    const p = document.createElement('p')
+    p.innerHTML = token
+    document.body.appendChild(p)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+messaging.onMessage(payload => {
+  console.log('Message received', payload)
+})
+
 if ('serviceWorker' in navigator &&
     (self.location.protocol === 'https:' || isLocalhost)) {
   navigator.serviceWorker.register('sw.js')
-    .then(registration => {})
+    .then(registration => {
+      messaging.useServiceWorker(registration)
+    })
     .catch(err => {
       console.log('注册 service worker 出现问题:', err)
     })
